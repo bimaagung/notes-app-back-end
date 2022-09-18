0ea1f859e117ff4cde3c74cd09b88b2672b9c49f
bimaagung <bimaagung.labs@gmail.com>
2022-09-18 17:06:15 +0700

    authentication implemented

--- src/server.js
+++ src/server.js
@@ -1,13 +1,29 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
+
+// notes
 const notes = require('./api/notes');
 // const NotesSevice = require('./services/inMemory/NoteService');
 const NotesSevice = require('./services/postgres/NoteService');
 const NotesValidator = require('./validator/notes');
 
+// users
+const users = require('./api/users');
+const UsersSevice = require('./services/postgres/UserService');
+const UsersValidator = require('./validator/users');
+
+// authentications
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
+const TokenManager = require('./tokenize/TokenManager');
+const AuthenticationsValidator = require('./validator/authentications');
+
 const init = async () => {
   const notesService = new NotesSevice();
+  const usersService = new UsersSevice();
+  const authenticationsService = new AuthenticationsService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -18,13 +34,59 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
+  // registrasi plugin eksternal
+  await server.register([
+    {
+      plugin: Jwt,
     },
+  ]);
+
+  // mendefinisikan strategy authentifikasi jwt
+  server.auth.strategy('notesapp_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
   });
+
+  await server.register(
+    [
+      {
+        plugin: notes,
+        options: {
+          service: notesService,
+          validator: NotesValidator,
+        },
+      },
+      {
+        plugin: users,
+        options:
+        {
+          service: usersService,
+          validator: UsersValidator,
+        },
+      },
+      {
+        plugin: authentications,
+        options:
+        {
+          authenticationsService,
+          usersService,
+          tokenManager: TokenManager,
+          validator: AuthenticationsValidator,
+        },
+      },
+    ],
+  );
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
